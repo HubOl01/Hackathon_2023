d147ca2addfe01769381316d34c3a895822ffa52
Oleg Kulichkov <kuloleg52@gmail.com>
2023-10-11 13:45:32 +0300

    +

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
+// import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_geocoder/geocoder.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:get/get.dart';
+import 'package:test_hackaton/pages/mapPage.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -18,90 +18,77 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        // useMaterial3: true,
-      ),
+          // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          // useMaterial3: true,
+          ),
       home: HomePage(),
     );
   }
 }
 
-
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-  LatLng point = LatLng(56.9972, 40.9714);
-class _HomePageState extends State<HomePage> {
-  @override
   Widget build(BuildContext context) {
-      double long = 49.5;
-  double lat = -0.09;
-  var location = [];
+     return GetBuilder<MyHomePageController>(
+        init: MyHomePageController(),
+        initState: (controller) {},
+        builder: (controller) {
+          return Scaffold(
+              body: IndexedStack(
+                index: controller.tabIndex.value,
+                children: pages,
+              ),
+              bottomNavigationBar: Obx(() => BottomNavigationBar(
+                    onTap: controller.changeTabIndex,
+                    // unselectedItemColor: context.theme.bottomNavigationBarTheme.unselectedItemColor,
+                    // selectedItemColor: Colors.redAccent,
+                    currentIndex: controller.tabIndex.value,
+                    iconSize: 30,
+                    showSelectedLabels: true,
+                    showUnselectedLabels: true,
+                    unselectedFontSize: 9,
+                    selectedFontSize: 9,
+                    enableFeedback: true,
+                    landscapeLayout:
+                        BottomNavigationBarLandscapeLayout.centered,
+                    type: BottomNavigationBarType.fixed,
+                    items: [
+                      const BottomNavigationBarItem(
+                          icon: Icon(Icons.calendar_month), label: "Календарь"),
+                      const BottomNavigationBarItem(
+                          icon: Icon(Icons.schedule), label: "Расписание пар"),
+                      BottomNavigationBarItem(
+                          icon: Icon(Icons.notes), label: "Заметки"),
+                      const BottomNavigationBarItem(
+                          icon: Icon(Icons.notes), label: "Заметки"),
+                      const BottomNavigationBarItem(
+                          icon: Icon(Icons.settings), label: "Настройки"),
+                    ],
+                  )));
+        });
+  }
+  
+}
+List<Widget> pages = [
+  MapPage(),
+  Scaffold(body: Center(child: Text(""),),)
+];
+List<String> pagesString = [
+  "Календарь",
+  "Расписание пар",
+  "Moodle",
+  "Заметки",
+  "Настройки",
+];
 
-    return Scaffold(
-      body: FlutterMap(
-    options: MapOptions(
-      center: LatLng(56.9972, 40.9714),
-      zoom: 3,
-      maxZoom: 18.49,
-      
-      onTap: (p, s) async {
-              location = await Geocoder.local.findAddressesFromCoordinates(
-                  new Coordinates(s.latitude, s.longitude));
+class MyHomePageController extends GetxController {
+  var tabIndex = 0.obs;
 
-              setState(() {
-                point = s;
-                print(s);
-              });
-
-              print(
-                  "${location.first.countryName} - ${location.first.addressLine}");
-            },
-    ),
-    nonRotatedChildren: [
-      // RichAttributionWidget(
-      //   attributions: [
-      //     TextSourceAttribution(
-      //       'OpenStreetMap contributors',
-      //       onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
-      //     ),
-      //   ],
-      // ),
-    ],
-    children: [
-      TileLayer(
-        urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-        // urlTemplate:'https://a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
-        
-        userAgentPackageName: 'com.example.app',
-        subdomains: ['a', 'b', 'c'],
-        maxNativeZoom: 20
-      ),
-      MarkerLayer(
-         markers: [
-                Marker(
-                  rotate: true,
-                  width: 30.0,
-                  height: 30.0,
-                  point: point,
-                  builder: (ctx) => InkWell(
-                    
-                    child: Container(
-                      // height: 10,
-                      // width: 10,
-                      color: Colors.black
-                      // child: 
-                    ),
-                  ),
-                )
-              ],
-      )
-    ],
-      )
-    );
+  void changeTabIndex(int index) {
+    print(index);
+    tabIndex.value = index;
+    update();
   }
 }
